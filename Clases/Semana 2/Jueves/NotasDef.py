6a7c3390c6ee9a0b5069c09b1199ec4617c26106
anabellajm <j.anabella@correo.unimet.edu.ve>
2023-01-21 17:54:01 -0400

    :)

--- Clases/Semana 2/Jueves/NotasDef.py
+++ Clases/Semana 2/Jueves/NotasDef.py
@@ -1,29 +1,65 @@
 print ('Hello! This program can help you calculate your final grade.')
 
-test_one = input('Please enter the grade of your first midterm: \n>>')
-while test_one.isnumeric() == False or float(test_one)>20:
-    test_one = input('\nPlease enter a valid number: \n>>')
-test_one = float(test_one)*0.3
-
-test_two = input ('Please enter the grade of your second midterm: \n>>')
-while test_two.isnumeric() == False or float(test_two)>20:
-    test_two = input('\nPlease enter a valid number: \n>>')
-test_two = float(test_two)*0.3
-
-quiz_one = input('Please enter the grade of your first quiz: \n>>')
-while quiz_one.isnumeric() == False or float(quiz_one)>20:
-    quiz_one = input('\nPlease enter a valid number: \n>>')
-quiz_one = float (quiz_one)*0.05
-
-quiz_two = input('Please enter the grade of your second quiz: \n>>')
-while quiz_two.isnumeric() == False or float(quiz_two)>20:
-    quiz_two = input('\nPlease enter a valid number: \n>>')
-quiz_two = float(quiz_two)*0.05
-
-proyect = input('Please enter the grade of your proyect: \n>>')
-while proyect.isnumeric() == False or float(proyect)>20:
-    proyect = input('\nPlease enter a valid number: \n>>')
-proyect = float(proyect)*0.3
+while True:
+    try:
+        test_one = float(input('Please enter the grade of your first midterm: \n>>'))
+        if (test_one)>20:
+            print('\nPlease enter a valid number between 0 and 20.')
+            continue
+        else:
+            test_one = float(test_one)*0.3 
+            break
+    except:
+        print('\nPlease enter a valid number between 0 and 20.')
+        continue 
+while True:
+    try:
+        test_two = float(input('\nPlease enter the grade of your second midterm: \n>>'))
+        if (test_two)>20:
+            print('\nPlease enter a valid number between 0 and 20.')
+            continue
+        else:
+            test_two = float(test_two)*0.3 
+            break
+    except:
+        print('\nPlease enter a valid number between 0 and 20.')
+        continue 
+while True:
+    try:
+        quiz_one = float(input('\nPlease enter the grade of your first quiz: \n>>'))
+        if (quiz_one)>20:
+            print('\nPlease enter a valid number between 0 and 20.')
+            continue
+        else:
+            quiz_one = float(quiz_one)*0.05 
+            break
+    except:
+        print('\nPlease enter a valid number between 0 and 20.')
+        continue 
+while True:
+    try:
+        quiz_two = float(input('\nPlease enter the grade of your second quiz: \n>>'))
+        if (quiz_two)>20:
+            print('\nPlease enter a valid number between 0 and 20.')
+            continue
+        else:
+            quiz_two = float(quiz_two)*0.05
+            break
+    except:
+        print('\nPlease enter a valid number between 0 and 20.')
+        continue
+while True:
+    try:
+        proyect = float(input('\nPlease enter the grade of your final proyect: \n>>'))
+        if (proyect)>20:
+            print('\nPlease enter a valid number between 0 and 20.')
+            continue
+        else:
+            proyect = float(proyect)*0.3
+            break
+    except:
+        print('\nPlease enter a valid number between 0 and 20.')
+        continue
 
 test_sums = test_one + test_two
 final_grade = test_one + test_two + quiz_one + quiz_two + proyect
